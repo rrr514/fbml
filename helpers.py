1b686ad585c41c81208de57b323a3cf5329b9bfe
Roy Yue <roy.yue05@gmail.com>
2025-06-06 11:18:12 -0500

    modified all training notebooks to save their projections as csv

--- helpers.py
+++ helpers.py
@@ -3,22 +3,22 @@ import pandas as pd
 # actual - 1d data frame
 # predicted - 1d numpy array
 # player - 1d data frame that matches the predictions to the player
-# display - T/F for displaying a preview of the results
-def compute_rank_squared_error(actual, predicted, player, display):
+def build_results_df(actual, predicted, player):
     results = pd.DataFrame({
         'Player': player,
         'Actual': actual.values,
         'Predicted': predicted
     }, index=actual.index)
 
+    return results
+
+# results - results df from build_results_df()
+def compute_rank_squared_error(results):
     results['ActualRank'] = results['Actual'].rank(ascending=False, method='min').astype(int)
     results['PredictedRank'] = results['Predicted'].rank(ascending=False, method='min').astype(int)
 
     results['RankError'] = results['PredictedRank'] - results['ActualRank']
 
     rank_squared_error = (results['RankError'] ** 2).sum()
-
-    if display:
-        print(results.to_string())
 
     return rank_squared_error
